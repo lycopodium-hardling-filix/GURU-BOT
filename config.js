9b61ea2b487cf2ef9b50589a9fe197abb5a97962
lycopodium-hardling-filix <91131291+lycopodium-hardling-filix@users.noreply.github.com>
2024-01-23 01:22:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['12892776303', 'filix bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
